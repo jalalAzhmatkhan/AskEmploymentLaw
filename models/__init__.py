3b50121717719358faa1f28a48b9ed3abe816449
jalalAzhmatkhan <jalaluddin.94@yahoo.com>
2025-01-19 08:24:24 +0700

    Add User-Role-Permission Models
    Add table models for User-Role-Permissions mapping

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from models.base_class import Base
+
+from models.tbl_permissions import TblPermissions
+from models.tbl_rolepermissions import TblRolePermissions
+from models.tbl_roles import TblRoles
+from models.tbl_users import TblUsers
+from models.tbl_user_role import TblUserRole
---
+++ models/tbl_users.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from models import Base
+
+class TblUsers(Base):
+    """
+    Tbl_Users to contain users' information
+    """
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=False)
+    user_role = relationship("TblUserRole", back_populates="user")
